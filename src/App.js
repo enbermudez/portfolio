869e4dcaed3435017ffc6891e99f7df802f05b9b
Enrique Bermúdez <enrique.bermudez@mercadolibre.com>
2021-07-14 14:14:20 -0300

    refactor: add data directory

--- src/App.js
+++ src/App.js
@@ -3,51 +3,7 @@ import TaskBar from './components/TaskBar';
 import DesktopGrid from './components/DesktopGrid';
 import Window from './components/Window';
 import DesktopIcon from './components/DesktopIcon';
-
-const sections = [
-  {
-    id: 1,
-    name: 'Información',
-    content: '¡Hola! Mi nombre es Enrique Bermúdez y este es mi portafolio. Soy ingenierio en computación y desarrollador web de 25 años.\n\nActualmente vivo en Santa Fe, Argentina y trabajo como desarrollador frontend en MercadoLibre.\n\nInvierto mi tiempo libre en ver series/películas, jugar (y programar) videojuegos, mirar partidos de fútbol y molestar a mi perra: Fiora.\n\nLas ventanas del portafolio funcionan como lo harían en Windows 98, así que puedes minimizarlas, maximizarlas, cerrarlas o incluso moverlas de un sitio a otro. ¡Los íconos del escritorio no están de adorno! Intenta hacer click en alguno de ellos.',
-    icon: 'user_card',
-  },
-  {
-    id: 2,
-    name: 'Contacto',
-    content: '---',
-    icon: 'msn',
-  },
-  {
-    id: 3,
-    name: 'Certificaciones',
-    content: '---',
-    icon: 'certificate',
-  },
-  {
-    id: 4,
-    name: 'Experiencia',
-    content: '---',
-    icon: 'script',
-  },
-  {
-    id: 5,
-    name: 'Proyectos',
-    content: '---',
-    icon: 'briefcase',
-  },
-  {
-    id: 6,
-    name: 'Tecnologías',
-    content: '---',
-    icon: 'console',
-  },
-  {
-    id: 7,
-    name: 'FAQ',
-    content: '---',
-    icon: 'help',
-  },
-];
+import sections from './data/sections';
 
 const App = () => {
   const [windows, setWindows] = useState([]);
